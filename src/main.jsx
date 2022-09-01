c82811932f533a3373311dba834a02ff6f12b740
devgustavosantos <devgustavosanto@gmail.com>
2022-09-01 16:42:03 -0300

    feat: create project theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { ThemeProvider } from "styled-components";
+import theme from "./styles/theme";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}></ThemeProvider>
   </React.StrictMode>
-)
+);
---
+++ src/styles/theme.js
@@ -0,0 +1,16 @@
+export default {
+  COLORS: {
+    BLACK_900: "#0D0C0F",
+    BLACK_800: "#1C1B1E",
+    BLACK_700: "#262529",
+
+    GRAY_900: "#3E3B47",
+    GRAY_800: "#948F99",
+
+    WHITE_900: "#CAC4CF",
+    WHITE_800: "#F4EDE8",
+
+    PINK_900: "#282124",
+    PINK_800: "#FF859B",
+  },
+};
